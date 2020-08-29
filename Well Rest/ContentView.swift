8c0efea5622951978a3474b928108806943d5c6a
Anurak Teerarattananukulchai <anurak.teera@gmail.com>
2020-08-29 17:43:07 +0700

    Initial Commit

---
+++ Well Rest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Well Rest
+//
+//  Created by anurak teerarattananukulchai on 29/8/2563 BE.
+//  Copyright © 2563 anurak teerarattananukulchai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
